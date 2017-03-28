f83ebe8827c1b02ec9094d6fd62a4d403869beef
Robert <robert.cebula1@gmail.com>
2017-03-28 22:29:02 +0200

    Initial commit

---
+++ examples/compile_and_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+   echo "Usage: $0 test_file.cpp [arguments_to_gtest]"
+   exit -1
+fi
+
+test_file_path=$1
+
+g++ -std=c++1y -Wall $test_file_path -lgtest && ./a.out "${@:2}"
---
+++ examples/p1/sum.h
@@ -0,0 +1,9 @@
+#ifndef _SUM_H
+#define _SUM_H
+
+int sum(int a, int b)
+{
+   return a + b;
+}
+
+#endif 

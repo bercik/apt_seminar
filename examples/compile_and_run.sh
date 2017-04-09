96a55ac46d2a35517d2d2fc3127959cf78b3518e
Robert <robert.cebula1@gmail.com>
2017-04-09 14:34:49 +0200

    Fix problem with double free or corruption

--- examples/compile_and_run.sh
+++ examples/compile_and_run.sh
@@ -7,5 +7,8 @@ fi
 
 test_file_path=$1
 
-g++ -std=c++1y -Wall $test_file_path -lgtest -lgmock -pthread \
+# for gtest only use -lgtest instead of -lgmock
+# usage of both would lead to double free or corruption error when using 
+# flags in gmock
+g++ -std=c++1y -Wall $test_file_path -lgmock -pthread \
     && ./a.out "${@:2}"
